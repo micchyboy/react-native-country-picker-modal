69ec0232a41763818626d705fcc8ac76156efa2c
Jethro Estrada <jethroestrada@kobe.local>
2017-05-03 14:06:28 +0800

    Removed letters

--- src/CountryPicker.js
+++ src/CountryPicker.js
@@ -329,15 +329,6 @@ export default class CountryPicker extends Component {
                     ) => this.setVisibleListHeight(offset)
                   }
                 />
-                <ScrollView
-                  contentContainerStyle={styles.letters}
-                  keyboardShouldPersistTaps="always"
-                >
-                  {
-                    this.state.filter === '' &&
-                    this.state.letters.map((letter, index) => this.renderLetters(letter, index))
-                  }
-                </ScrollView>
               </View>
             </KeyboardAvoidingView>
           </View>
